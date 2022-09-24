fe7408876c5cb1483c4bd2c2dcb954806c1cb01f
LightningItself <68198395+LightningItself@users.noreply.github.com>
2022-09-24 19:26:15 +0530

    exercise 1.1-1.2

---
+++ courseinfo/src/App.js
@@ -0,0 +1,53 @@
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+};
+
+const Content = ({ parts, exercises }) => {
+  return (
+    <div>
+      <Part part={parts.part1} exercises={exercises.exercises1} />
+      <Part part={parts.part2} exercises={exercises.exercises2} />
+      <Part part={parts.part3} exercises={exercises.exercises3} />
+    </div>
+  );
+};
+
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  );
+};
+
+const Total = ({ exercises }) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {exercises.exercises1 + exercises.exercises2 + exercises.exercises3}
+    </p>
+  );
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        parts={{ part1, part2, part3 }}
+        exercises={{ exercises1, exercises2, exercises3 }}
+      />
+      <Total exercises={{ exercises1, exercises2, exercises3 }} />
+    </div>
+  );
+};
+
+export default App;
