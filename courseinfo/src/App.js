f852275fb5b51bdd1890b6aee08d7f197c720c78
LightningItself <68198395+LightningItself@users.noreply.github.com>
2022-09-24 19:49:58 +0530

    exercise 1.3-1.5

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -1,51 +1,59 @@
 const Header = ({ course }) => {
-  return <h1>{course}</h1>;
+  return <h1>{course.name}</h1>;
 };
 
-const Content = ({ parts, exercises }) => {
+const Content = ({ course }) => {
   return (
     <div>
-      <Part part={parts.part1} exercises={exercises.exercises1} />
-      <Part part={parts.part2} exercises={exercises.exercises2} />
-      <Part part={parts.part3} exercises={exercises.exercises3} />
+      <Part part={course.parts[0]} />
+      <Part part={course.parts[1]} />
+      <Part part={course.parts[2]} />
     </div>
   );
 };
 
-const Part = ({ part, exercises }) => {
+const Part = ({ part }) => {
   return (
     <p>
-      {part} {exercises}
+      {part.name} {part.exercises}
     </p>
   );
 };
 
-const Total = ({ exercises }) => {
+const Total = ({ course }) => {
+  const parts = course.parts;
   return (
     <p>
       Number of exercises{" "}
-      {exercises.exercises1 + exercises.exercises2 + exercises.exercises3}
+      {parts[0].exercises + parts[1].exercises + parts[2].exercises}
     </p>
   );
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        parts={{ part1, part2, part3 }}
-        exercises={{ exercises1, exercises2, exercises3 }}
-      />
-      <Total exercises={{ exercises1, exercises2, exercises3 }} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   );
 };
