0893a1fbfd668c665f688b8d1fe8aea5cb7accaa
LightningItself <68198395+LightningItself@users.noreply.github.com>
2022-09-30 02:48:37 +0530

    phonebook complete

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
+const Person = require("./models/person");
+require("dotenv").config();
+
+const url = process.env.MONGODB_URI;
+
+console.log("Connecting to MongoDB Atlas server");
+mongoose
+  .connect(url)
+  .then((res) => console.log("MongoDB connected"))
+  .catch((error) =>
+    console.log("Failed to connect to MongoDB with error", error.message)
+  );
 
 app.use(express.json());
 const requestLogger = (req, res, next) => {
@@ -13,74 +26,76 @@ const requestLogger = (req, res, next) => {
 app.use(requestLogger);
 app.use(cors());
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
-let db = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4,
-  },
-  {
-    name: "IG",
-    number: "39-23-6423122",
-    id: 5,
-  },
-];
-
-app.get("/api/persons", (req, res) => {
-  res.json(db);
+app.get("/api/persons", async (req, res) => {
+  const persons = await Person.find({});
+  res.json(persons);
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  const person = db[req.params.id - 1];
-  res.json(person);
-});
-
-app.delete("/api/persons/:id", (req, res) => {
-  const person = db[req.params.id - 1];
-  if (person) {
-    db = db.filter((entry) => entry.id !== person.id);
-    res.status(204).end();
-  } else {
-    console.log("person already removed");
-    res.status(400).json({ error: "id not found" });
+app.get("/api/persons/:id", async (req, res, next) => {
+  try {
+    const person = await Person.findOne({
+      _id: mongoose.Types.ObjectId(req.params.id),
+    });
+    res.json(person);
+  } catch (error) {
+    next(error);
   }
 });
 
-app.post("/api/persons", (req, res) => {
+app.delete("/api/persons/:id", async (req, res) => {
+  const personId = req.params.id;
+  await Person.deleteOne({ _id: mongoose.Types.ObjectId(personId) });
+  res.status(204).end();
+});
+
+app.post("/api/persons", async (req, res) => {
   body = req.body;
   const newPerson = {
     name: body.name,
     number: body.number,
-    id: db.length + 1,
   };
-
-  if (db.find((person) => person.name === newPerson.name)) {
+  existingPerson = await Person.findOne({ name: newPerson.name });
+  console.log(existingPerson);
+  if (existingPerson !== null) {
     res.status(400).json({ error: "Person already exists" });
-  }
-  if (newPerson.name !== "" && newPerson.number !== "") {
-    db = db.concat(newPerson);
-    res.json(newPerson);
+  } else if (newPerson.name !== "" && newPerson.number !== "") {
+    const person = new Person({ ...newPerson });
+    person.save().then((result) => res.json(newPerson));
   } else {
     res.status(400).json({ error: "Name or number missing" });
   }
 });
+
+app.put("/api/persons/:id", async (req, res) => {
+  const body = req.body;
+  const filter = { _id: mongoose.Types.ObjectId(req.params.id) };
+  const update = { number: body.number };
+  if (body.number) {
+    const response = await Person.findOneAndUpdate(filter, update, {
+      new: true,
+    });
+    res.status(204).end();
+  } else {
+    res.status(400).end();
+  }
+});
+
+const errorHandler = (err, req, res, next) => {
+  console.error(err.message);
+  if (err.name == "BSONTypeError") {
+    return res.status(400).send({ error: "malformatted id" });
+  }
+  next(err);
+};
+
+app.use(errorHandler);
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "Unknown Endpoint" });
+};
+app.use(unknownEndpoint);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- phonebook_backend/models/person.js
+++ phonebook_backend/models/person.js
@@ -1,16 +1,5 @@
 const mongoose = require("mongoose");
 
-const url = process.env.MONGODB_URI;
-
-console.log("Connecting to MongoDB Atlas server");
-
-mongoose
-  .connect(url)
-  .then((res) => console.log("MongoDB connected"))
-  .catch((error) =>
-    console.log("Failed to connect to MongoDB with error", error.message)
-  );
-
 const personSchema = new mongoose.Schema({
   name: String,
   number: String,
